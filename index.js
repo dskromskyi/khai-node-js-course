c2b66fd487c6cec13ca113285598a22c367c71c8
Dima K <d.s.kromskyi@student.khai.edu>
2025-12-12 13:09:59 +0200

    add error handling middleware and error route

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-// index.js
 const express = require('express');
 const productRoutes = require('./product.routes');
 const { logRequest } = require('./middleware');
+const { errorResponder } = require('./error.middleware');
 
 const app = express();
 const PORT = 3000;
 
-// Apply middleware globally
 app.use(logRequest);
-
-// Use product routes
 app.use(productRoutes);
+
+// Error handling middleware must go after routes
+app.use(errorResponder);
 
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`);
